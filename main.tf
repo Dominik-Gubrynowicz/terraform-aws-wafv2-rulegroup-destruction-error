d084dfeb2880698629d04bd2834da0cced721ee7
Dominik Gubrynowicz <dominik.gubrynowicz@gmail.com>
2022-12-13 17:43:45 +0100

    feat: remove first rule group

--- main.tf
+++ main.tf
@@ -1,36 +1,36 @@
-resource "aws_wafv2_rule_group" "example_rule_group1" {
-  name     = "example-rule1"
-  scope    = "REGIONAL"
-  capacity = 2
+# resource "aws_wafv2_rule_group" "example_rule_group1" {
+#   name     = "example-rule1"
+#   scope    = "REGIONAL"
+#   capacity = 2
 
-  rule {
-    name     = "rule-1"
-    priority = 1
+#   rule {
+#     name     = "rule-1"
+#     priority = 1
 
-    action {
-      allow {}
-    }
+#     action {
+#       allow {}
+#     }
 
-    statement {
+#     statement {
 
-      geo_match_statement {
-        country_codes = ["US", "NL"]
-      }
-    }
+#       geo_match_statement {
+#         country_codes = ["US", "NL"]
+#       }
+#     }
 
-    visibility_config {
-      cloudwatch_metrics_enabled = false
-      metric_name                = "friendly-rule-metric-name"
-      sampled_requests_enabled   = false
-    }
-  }
+#     visibility_config {
+#       cloudwatch_metrics_enabled = false
+#       metric_name                = "friendly-rule-metric-name"
+#       sampled_requests_enabled   = false
+#     }
+#   }
 
-  visibility_config {
-    cloudwatch_metrics_enabled = false
-    metric_name                = "friendly-metric-name"
-    sampled_requests_enabled   = false
-  }
-}
+#   visibility_config {
+#     cloudwatch_metrics_enabled = false
+#     metric_name                = "friendly-metric-name"
+#     sampled_requests_enabled   = false
+#   }
+# }
 
 resource "aws_wafv2_rule_group" "example_rule_group2" {
   name     = "example-rule2"
@@ -76,26 +76,26 @@ resource "aws_wafv2_web_acl" "example" {
     allow {}
   }
 
-  rule {
-    name     = "rule-1"
-    priority = 1
+  # rule {
+  #   name     = "rule-1"
+  #   priority = 1
 
-    override_action {
-      count {}
-    }
+  #   override_action {
+  #     count {}
+  #   }
 
-    statement {
-      rule_group_reference_statement {
-        arn = aws_wafv2_rule_group.example_rule_group1.arn
-      }
-    }
+  #   statement {
+  #     rule_group_reference_statement {
+  #       arn = aws_wafv2_rule_group.example_rule_group1.arn
+  #     }
+  #   }
 
-    visibility_config {
-      cloudwatch_metrics_enabled = false
-      metric_name                = "friendly-rule-metric-name"
-      sampled_requests_enabled   = false
-    }
-  }
+  #   visibility_config {
+  #     cloudwatch_metrics_enabled = false
+  #     metric_name                = "friendly-rule-metric-name"
+  #     sampled_requests_enabled   = false
+  #   }
+  # }
 
   rule {
     name     = "rule-2"
